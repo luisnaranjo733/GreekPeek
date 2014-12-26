fa2f2b75bd6b91b8d8de2eeb15255c1794287882
Luis Naranjo <luisnaranjo733@gmail.com>
2014-12-25 18:28:25 -0800

    Rebooting GP. Static only prototype coming soon

--- network/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from network.models import *
-
-admin.site.register(User)
-admin.site.register(Recruit)
-admin.site.register(Chapter)
-admin.site.register(RushChair)
